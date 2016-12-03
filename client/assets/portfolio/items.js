d8d101633a0c768d98037ffe57f360c1b142a6bc
Adonis Tumann <adonistuman@gmail.com>
2016-12-02 23:41:36 -0800

    Revert

--- client/assets/portfolio/items.js
+++ client/assets/portfolio/items.js
@@ -3,15 +3,13 @@ import CreedEsports from './creedesports/item.js'
 import DailyUI from './dailyui/item.js'
 import HealthyHabits from './healthyhabits/item.js'
 import Void from './void/item.js'
-import TestItem from './test/item.js'
 
 const portfolio = [
   AFL,
   CreedEsports,
   DailyUI,
   HealthyHabits,
-  Void,
-  TestItem
+  Void
 ]
 
 export default portfolio
