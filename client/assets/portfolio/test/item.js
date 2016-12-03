a4096f5d14c386bcd65e3fc0a0656732b2c54ac7
Adonis Tumann <adonistuman@gmail.com>
2016-12-02 23:39:41 -0800

    added portfolio item

---
+++ client/assets/portfolio/test/item.js
@@ -0,0 +1,13 @@
+const item = {
+  title: 'Test',
+  type: 'abstract',
+  description: 'this is a new portfolio item',
+  cover: require('./images/test.png'),
+  images: [
+    require('./images/test1.jpg'),
+    require('./images/test2.jpg'),
+
+  ]
+}
+
+export default item
